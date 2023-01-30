e82d0a43a17bb2eedab2bff700573327149e8d71
ggrangia <giacomo.grangia@gmail.com>
2023-01-30 23:34:01 +0100

    Complete RAM sharing

--- dns-resolver/locals.tf
+++ dns-resolver/locals.tf
@@ -1,3 +1,5 @@
 locals {
   escaped_domain_name = replace(var.private_domain, ".", "_")
+  extra_rules_arn     = [for r in aws_route53_resolver_rule.extra : r.arn]
+  all_rules           = concat([aws_route53_resolver_rule.mydomain.arn], local.extra_rules_arn)
 }
--- dns-resolver/ram.tf
+++ dns-resolver/ram.tf
@@ -4,13 +4,14 @@ resource "aws_ram_resource_share" "rules" {
   allow_external_principals = false // share with your org only
 }
 
-// FIXME: Add all the rules to the share
+// Add all the rules to the share
 resource "aws_ram_resource_association" "rules" {
-
+    for_each = toset(local.all_rules)
+    resource_arn = each.value
+    resource_share_arn = aws_ram_resource_share.rules.arn
 }
 
 resource "aws_ram_principal_association" "rules_org" {
-  # FIXME: fetch org
   principal = data.aws_organizations_organization.myorg.id
   resource_share_arn = aws_ram_resource_share.rules.arn
 }
