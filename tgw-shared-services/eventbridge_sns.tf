999a3c19f7afcca678e701f8364ff84a6c62a9ec
Giacomo Grangia <giacomo.grangia@gmail.com>
2023-04-14 00:26:31 +0200

    Create role for sns lambda_failure_feedback_role_arn

--- tgw-shared-services/eventbridge_sns.tf
+++ tgw-shared-services/eventbridge_sns.tf
@@ -2,9 +2,9 @@ resource "aws_sns_topic" "network_manager" {
   provider = aws.netmanager
 
   name                                = "network_manager"
-  lambda_failure_feedback_role_arn    = "arn:aws:iam::605665581171:role/SNSFailureFeedback"
-  lambda_success_feedback_role_arn    = "arn:aws:iam::605665581171:role/SNSSuccessFeedback"
-  lambda_success_feedback_sample_rate = 100
+  lambda_failure_feedback_role_arn    = aws_iam_role.sns_logs.arn
+  lambda_success_feedback_role_arn    = aws_iam_role.sns_logs.arn
+  lambda_success_feedback_sample_rate = 0
 }
 
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
@@ -13,4 +13,40 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.network_manager.arn
   protocol  = "lambda"
   endpoint  = module.lambda_tgw_accept_attachment.lambda_function_arn
+}
+
+resource "aws_iam_role" "sns_logs" {
+  provider = aws.netmanager
+  name     = "sns_logs"
+
+  assume_role_policy = data.aws_iam_policy_document.trusted_entity_sns_logs.json
+  inline_policy {
+    name   = "inline_sns"
+    policy = data.aws_iam_policy_document.sns_logs.json
+  }
+}
+
+data "aws_iam_policy_document" "sns_logs" {
+  provider = aws.netmanager
+  statement {
+    actions = ["logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:PutMetricFilter",
+    "logs:PutRetentionPolicy"]
+
+    resources = ["*"]
+  }
+}
+
+
+data "aws_iam_policy_document" "trusted_entity_sns_logs" {
+  provider = aws.netmanager
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["sns.amazonaws.com"]
+    }
+  }
 }
