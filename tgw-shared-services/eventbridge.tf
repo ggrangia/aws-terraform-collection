02405043cfaf82336425be572f43eabde58de8b5
Giacomo Grangia <giacomo.grangia@gmail.com>
2023-04-14 00:25:23 +0200

    Remove sqs

--- tgw-shared-services/eventbridge.tf
+++ tgw-shared-services/eventbridge.tf
@@ -27,62 +27,6 @@ resource "aws_cloudwatch_event_target" "netman_log_group" {
   arn  = aws_cloudwatch_log_group.customNetManagerLogGroup.arn
 }
 
-resource "aws_sqs_queue" "network_manager_queue" {
-  provider = aws.netmanager
-
-  name                       = "network_manager_queue"
-  visibility_timeout_seconds = 30
-  message_retention_seconds  = 86400 # 1 day
-  receive_wait_time_seconds  = 0
-}
-
-
-resource "aws_cloudwatch_event_target" "netman_sqs" {
-  provider = aws.netmanager
-
-  rule = aws_cloudwatch_event_rule.networkmanager.name
-  arn  = aws_sqs_queue.network_manager_queue.arn
-}
-
-data "aws_iam_policy_document" "cross_sqs" {
-  provider = aws.netmanager
-
-  statement {
-    sid = "AllowNetworkEvents"
-
-    actions = ["sqs:SendMessage"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["events.amazonaws.com"]
-    }
-
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values   = [aws_cloudwatch_event_rule.networkmanager.arn]
-    }
-  }
-
-  statement {
-    sid = "AllowLambda"
-
-    actions = ["sqs:*"]
-    principals {
-      type        = "AWS"
-      identifiers = [module.lambda_tgw_accept_attachment.lambda_role_arn]
-    }
-    resources = [aws_sqs_queue.network_manager_queue.arn]
-  }
-}
-
-resource "aws_sqs_queue_policy" "cross_sqs" {
-  provider = aws.netmanager
-
-  queue_url = aws_sqs_queue.network_manager_queue.id
-  policy    = data.aws_iam_policy_document.cross_sqs.json
-}
-
 resource "aws_cloudwatch_event_target" "netman_sns" {
   provider = aws.netmanager
 
--- tgw-shared-services/lambda.tf
+++ tgw-shared-services/lambda.tf
@@ -12,32 +12,11 @@ module "lambda_tgw_accept_attachment" {
     "arn:aws:lambda:eu-west-1:017000801446:layer:AWSLambdaPowertoolsPythonV2:26",
   ]
 
-  attach_policy_json = true
-  policy_json        = data.aws_iam_policy_document.netman_sqs.json
-
   tags = {
     Name = "tgw_accept_attachment"
   }
 }
 
-data "aws_iam_policy_document" "netman_sqs" {
-  statement {
-    sid = "ReadFromSqs"
-
-    actions = [
-      "sqs:DeleteMessage",
-      "sqs:ChangeMessageVisibility",
-      "sqs:ReceiveMessage",
-      "sqs:GetQueueAttributes"
-    ]
-
-    resources = [
-      aws_sqs_queue.network_manager_queue.arn,
-      #"arn:aws:sqs:us-east-2:${data.aws_caller_identity.netmanager.account_id}:network_manager_queue"
-    ]
-  }
-
-}
 
 resource "aws_lambda_permission" "sns_network_manager" {
   statement_id_prefix = "sns_network_manager"
