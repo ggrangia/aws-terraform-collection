b474edd997625a07cbb85d779c3f7de557713fc7
Giacomo Grangia <giacomo.grangia@gmail.com>
2023-04-14 22:21:10 +0200

    Specific eb rule for vpc-attachment-created

--- tgw-shared-services/eventbridge.tf
+++ tgw-shared-services/eventbridge.tf
@@ -1,10 +1,15 @@
-resource "aws_cloudwatch_event_rule" "networkmanager" {
+resource "aws_cloudwatch_event_rule" "vpc_attachment_created" {
   provider    = aws.netmanager
-  name        = "capture-networkmanager"
+  name        = "vpc_attachment_created"
   description = "Capture each Network Manager event"
 
   event_pattern = jsonencode({
-    "source" : ["aws.networkmanager"]
+    "source" : ["aws.networkmanager"],
+    "detail-type" : ["Network Manager Topology Change"],
+    "detail" : {
+      "changeType" : [{ "equals-ignore-case" : "vpc-attachment-created" }],
+      "region" : [{ "equals-ignore-case" : "eu-west-1" }]
+    }
   })
 
   depends_on = [
@@ -12,24 +17,24 @@ resource "aws_cloudwatch_event_rule" "networkmanager" {
   ]
 }
 
-resource "aws_cloudwatch_log_group" "customNetManagerLogGroup" {
+resource "aws_cloudwatch_log_group" "vpc_attachment_created" {
   provider = aws.netmanager
 
-  name              = "/aws/events/customNetManagerLogGroup"
+  name              = "/aws/events/vpcAttachmentCreatedLogGroup"
   retention_in_days = 7
 
 }
 
-resource "aws_cloudwatch_event_target" "netman_log_group" {
+resource "aws_cloudwatch_event_target" "vpc_attachment_created_log_group" {
   provider = aws.netmanager
 
-  rule = aws_cloudwatch_event_rule.networkmanager.name
-  arn  = aws_cloudwatch_log_group.customNetManagerLogGroup.arn
+  rule = aws_cloudwatch_event_rule.vpc_attachment_created.name
+  arn  = aws_cloudwatch_log_group.vpc_attachment_created.arn
 }
 
-resource "aws_cloudwatch_event_target" "netman_sns" {
+resource "aws_cloudwatch_event_target" "vpc_attachment_created_sns" {
   provider = aws.netmanager
 
-  rule = aws_cloudwatch_event_rule.networkmanager.name
+  rule = aws_cloudwatch_event_rule.vpc_attachment_created.name
   arn  = aws_sns_topic.network_manager.arn
 }
