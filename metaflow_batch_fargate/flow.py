1ed621f8a35677583dc039366097658c416af11e
ggrangia <giacomo.grangia@gmail.com>
2024-05-15 22:43:59 +0200

    Update

--- metaflow_batch_fargate/flow.py
+++ metaflow_batch_fargate/flow.py
@@ -6,7 +6,7 @@ class MyFlow(FlowSpec):
 
     # memory in MiB (1GB = 1024MiB)
     # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html
-    @resources(memory=2048, cpu=1)
+    # @resources(memory=2048, cpu=1)
     @step
     def start(self):
         self.my_var = "hello world"
@@ -25,23 +25,41 @@ class MyFlow(FlowSpec):
 
     @step
     def branch_a(self):
+        self.my_var = self.my_var + "a"
         print(f"Branching a: {self.my_var}")
         self.next(self.aggregator)
 
     @step
     def branch_b(self):
+        self.my_var = self.my_var + "b"
         print(f"Branching b: {self.my_var}")
         self.next(self.aggregator)
 
     @step
     def aggregator(self, inputs):
-        self.my_var = inputs.branch_a.my_var + inputs.branch_b.my_var
+        self.my_var = [inputs.branch_a.my_var, inputs.branch_b.my_var]
         print(f"Aggregating.. {self.my_var}")
+
+        self.next(self.prepare_each)
+
+    @step
+    def prepare_each(self):
+        self.next(self.many_each, foreach="my_var")
+
+    @step
+    def many_each(self):
+        print(f"In foreach: {self.input}")
+        self.tmp = self.input
+        self.next(self.join_each)
+
+    @step
+    def join_each(self, inputs):
+        self.result = [input.tmp for input in inputs]
         self.next(self.end)
 
     @step
     def end(self):
-        print(f"the data artifact is still: {self.my_var}")
+        print(f"the final artifact is : {self.result}")
         pass
 
 
--- metaflow_batch_fargate/vpc.tf
+++ metaflow_batch_fargate/vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.7"
 
   name = "eoliann-vpc"
   cidr = "10.0.0.0/16"
@@ -14,4 +15,50 @@ module "vpc" {
   one_nat_gateway_per_az = false
 
   enable_vpn_gateway = false
+}
+
+module "vpc_v6" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.7"
+
+  name = "eoliann-vpc-v6"
+
+  azs         = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  enable_ipv6 = true
+
+  public_subnet_ipv6_native    = true
+  public_subnet_ipv6_prefixes  = [0, 1, 2]
+  private_subnet_ipv6_native   = true
+  private_subnet_ipv6_prefixes = [3, 4, 5]
+
+  # RDS currently only supports dual-stack so IPv4 CIDRs will need to be provided for subnets
+  # database_subnet_ipv6_native   = true
+  # database_subnet_ipv6_prefixes = [6, 7, 8]
+  enable_nat_gateway = false
+
+  create_egress_only_igw = true
+
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
+  assign_generated_ipv6_cidr_block = true
+  tags = {
+    Name = "main"
+  }
+}
+
+
+resource "aws_subnet" "subv6" {
+
+  vpc_id = aws_vpc.main.id
+
+  ipv6_cidr_block                                = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 1)
+  assign_ipv6_address_on_creation                = true # necessary for ipv6 native subnet
+  ipv6_native                                    = true
+  enable_resource_name_dns_aaaa_record_on_launch = true
 }
