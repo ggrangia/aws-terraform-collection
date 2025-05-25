30a81abd32ca0b3dbcdc35032244de18be23ed2d
ggrangia <giacomo.grangia@gmail.com>
2025-05-25 23:10:18 +0800

    Added remark on cognito jwt issuer format

--- entraid-federation/20_entraid/main.tf
+++ entraid-federation/20_entraid/main.tf
@@ -8,6 +8,7 @@ resource "azuread_application_federated_identity_credential" "aws_integration" {
   display_name   = var.entraid_credentials_name
   description    = "Federation with AWS Identity Pools created in this example"
   audiences      = [data.terraform_remote_state.cognito.outputs.cognito_pool_id]
-  issuer         = "https://cognito-identity.amazonaws.com"
-  subject        = "add-here-cognito-identity-id" # Add here the aws cognito identity id  e.g. "{region}:{uuid}"
+  # If not in us-east-1, it might have the region before amazonaws.com. Check the issuer from the token itself
+  issuer  = "https://cognito-identity.amazonaws.com"
+  subject = "add-here-cognito-identity-id" # Add here the aws cognito identity id  e.g. "{region}:{uuid}"
 }
