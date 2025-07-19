a2991bfc1174aa2e73e34365f0f417d5842dd74a
ggrangia <giacomo.grangia@gmail.com>
2025-07-19 12:57:47 +0800

    Final pre-commit tflint checks

--- centralized-internet-egress/spoke_vpc/main.tf
+++ centralized-internet-egress/spoke_vpc/main.tf
@@ -44,13 +44,13 @@ resource "aws_route_table" "private" {
 
 resource "aws_route_table_association" "private" {
   for_each       = toset(data.aws_availability_zones.available.names)
-  subnet_id      = (lookup(aws_subnet.pvt, each.value)).id
+  subnet_id      = (lookup(aws_subnet.pvt, each.value, "")).id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "tgw" {
   for_each       = toset(data.aws_availability_zones.available.names)
-  subnet_id      = (lookup(aws_subnet.tgw_eni, each.value)).id
+  subnet_id      = (lookup(aws_subnet.tgw_eni, each.value, "")).id
   route_table_id = aws_route_table.private.id
 }
 
