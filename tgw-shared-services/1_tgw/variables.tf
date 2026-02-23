afb99488dd6babc829f03e7c24a028fa27abbb19
ggrangia <giacomo.grangia@gmail.com>
2026-02-23 21:20:55 +0800

    Improve description for private_asn var

--- tgw-shared-services/1_tgw/variables.tf
+++ tgw-shared-services/1_tgw/variables.tf
@@ -5,5 +5,5 @@ variable "transit_gateway_rt_names" {
 
 variable "private_asn" {
   type        = number
-  description = "The private ASN for the Amazon side of a BGP session"
+  description = "The private ASN for the Amazon side of a BGP session (64512 to 65534 32 bits or 4200000000 to 4294967294 64 bits)"
 }
