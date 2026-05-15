e37fd93f4a8ca1deb2dd36d25b431202b095346c
ggrangia <giacomo.grangia@gmail.com>
2026-05-16 00:10:35 +0800

    Add EKS module configuration with workload subnet IDs and Karpenter setup

---
+++ eks-setup/main.tf
@@ -0,0 +1,78 @@
+output "vpc" {
+  value = module.vpc.private_subnets
+}
+
+locals {
+  eks_workload_subnet_ids = [for k, v in module.vpc.private_subnets : v["id"] if startswith(k, "workload")]
+  name                    = "my-eks"
+}
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  create = var.create_vpc
+
+  name = local.name
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = local.eks_workload_subnet_ids
+  control_plane_subnet_ids = local.eks_workload_subnet_ids
+
+  kubernetes_version = "1.35"
+
+  endpoint_private_access = true
+  endpoint_public_access  = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  compute_config = {
+    enabled = false
+  }
+
+  // TODO: enabled_log_types
+
+  create_node_iam_role       = false
+  create_node_security_group = false
+
+  addons = {
+    coredns    = {}
+    kube-proxy = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    vpc-cni = {
+      before_compute = true
+      configuration_values = jsonencode({
+        env = {
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
+  }
+
+
+  eks_managed_node_groups = {
+    karpenter = {
+      ami_type       = "BOTTLEROCKET_x86_64"
+      instance_types = ["m5.large"]
+
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
+
+      labels = {
+        # Used to ensure Karpenter runs on nodes that it does not manage
+        "karpenter.sh/controller" = "true"
+      }
+    }
+  }
+  // TODO: check how to pass roles to nodes, also with karpenter
+
+
+  node_security_group_tags = {
+    "karpenter.sh/discovery" = local.name
+  }
+}
